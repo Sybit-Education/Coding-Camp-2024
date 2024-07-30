3940ea0cb80c132936d9bb265f93e96fa220bce6
ThiaraAdrian <thiara.adrian@gmail.com>
2024-07-30 09:56:54 +0200

    Bilder hinzugefügt

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AsyncPipe} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import {ActivatedRoute, RouterLink} from "@angular/router";
+import { AirtableService } from '../../services/airtable.service';
+import { Activity } from '../../types/activity.interface';
 
 @Component({
   selector: 'app-detail',
@@ -12,6 +14,20 @@ import {RouterLink} from "@angular/router";
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss'
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
 
+  activity: Activity | null = null  
+
+  constructor(
+    private route: ActivatedRoute, 
+    private activityService: AirtableService
+  ) {}
+  ngOnInit(): void {
+    const osmId = this.route.snapshot.paramMap.get('osm_id'); 
+    if (osmId) {
+      this.activityService.getActivitiesByOsmId(osmId).subscribe ((activities)=>{
+        this.activity = activities[0]
+      })
+    }
+  }
 }
