d3715b553c5b1c4d3c1cf8f71dc07d5b1e6d0ebf
Stein <86646466+IamSTEINI@users.noreply.github.com>
2024-07-30 11:18:59 +0200

    Added cards for main-ui

---
+++ src/app/components/activity-card/activity-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './activity-card.component.html',
+  styleUrl: './activity-card.component.scss'
+})
+export class ActivityCardComponent {
+  
+}
