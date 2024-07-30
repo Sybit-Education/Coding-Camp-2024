4476b9956504f0f89143fd5942eb9abcf9329a9f
Isa308 <perzisabell@gmail.com>
2024-07-30 15:18:09 +0200

    fix unused imports

--- src/app/components/feed-settings/feed-settings.component.ts
+++ src/app/components/feed-settings/feed-settings.component.ts
@@ -1,9 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FilterComponent } from '../filter/filter.component';
-import { combineLatest, switchMap, take, map, BehaviorSubject, Observable } from 'rxjs';
-import { TypesInterface } from '../../types/types.interface';
-import { AirtableService } from '../../services/airtable.service';
-import { Activity } from '../../types/activity.interface';
 
 @Component({
   selector: 'app-feed-settings',
