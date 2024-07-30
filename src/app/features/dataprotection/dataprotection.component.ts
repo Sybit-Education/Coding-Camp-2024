9d216659891817a06d916b132a2dd432991e4706
maxkuzzmenko1 <maxkuzzmenko3578@protonmail.com>
2024-07-30 10:44:25 +0200

    Datenschutz
    Co-Authored-By: maxkuzzmenko <127974451+maxkuzzmenko@users.noreply.github.com>
    Co-Authored-By: jonasnebel3909 <176913962+jonasnebel3909@users.noreply.github.com>

---
+++ src/app/features/dataprotection/dataprotection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dataprotection',
+  standalone: true,
+  imports: [],
+  templateUrl: './dataprotection.component.html',
+  styleUrl: './dataprotection.component.scss'
+})
+export class DataprotectionComponent {
+
+}
