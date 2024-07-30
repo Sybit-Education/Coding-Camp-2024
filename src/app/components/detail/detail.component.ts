2356f761a13f4d7faa16ad164f936023243e9ce9
Alexander, Kugler <alexander.kugler@sybit.de>
2024-07-30 10:16:32 +0200

    fix: routing

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,33 +1,34 @@
 import {Component, OnInit} from '@angular/core';
-import {AsyncPipe} from "@angular/common";
+import {AirtableService} from "../../services/airtable.service";
+import {AsyncPipe, NgStyle} from "@angular/common";
+import {Activity} from "../../types/activity.interface";
 import {ActivatedRoute, RouterLink} from "@angular/router";
-import { AirtableService } from '../../services/airtable.service';
-import { Activity } from '../../types/activity.interface';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
   imports: [
     AsyncPipe,
-    RouterLink
+    RouterLink,
+    NgStyle
   ],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss'
 })
 export class DetailComponent implements OnInit {
-
-  activity: Activity | null = null  
+  activity: Activity | null = null;
 
   constructor(
-    private route: ActivatedRoute, 
+    private route: ActivatedRoute,
     private activityService: AirtableService
   ) {}
+
   ngOnInit(): void {
-    const osmId = this.route.snapshot.paramMap.get('osm_id'); 
+    const osmId = this.route.snapshot.paramMap.get('osm_id');
     if (osmId) {
-      this.activityService.getActivitiesByOsmId(osmId).subscribe ((activities)=>{
-        this.activity = activities[0]
-      })
+      this.activityService.getActivitiesByOsmId(osmId).subscribe((activities) => {
+        this.activity = activities[0];
+      });
     }
   }
 }
