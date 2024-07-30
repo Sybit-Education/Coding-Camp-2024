48baed9ed36ff3a769f0373c3f11f44cb70c6f6b
Stein <86646466+IamSTEINI@users.noreply.github.com>
2024-07-30 13:07:24 +0200

    Added navbar & Home feed btns

---
+++ src/app/components/feed-settings/feed-settings.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FeedSettingsComponent } from './feed-settings.component';
+
+describe('FeedSettingsComponent', () => {
+  let component: FeedSettingsComponent;
+  let fixture: ComponentFixture<FeedSettingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FeedSettingsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FeedSettingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -8,6 +8,8 @@ import {ListComponent} from "../../components/list/list.component";
 import {AsyncPipe} from "@angular/common";
 import {FooterComponent} from "../../components/footer/footer.component";
 import { ActivityCardComponent } from '../../components/activity-card/activity-card.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FeedSettingsComponent } from '../../components/feed-settings/feed-settings.component';
 
 @Component({
   selector: 'app-home',
@@ -21,7 +23,9 @@ import { ActivityCardComponent } from '../../components/activity-card/activity-c
     ListComponent,
     AsyncPipe,
     FooterComponent,
-    ActivityCardComponent
+    ActivityCardComponent,
+    NavbarComponent,
+    FeedSettingsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
