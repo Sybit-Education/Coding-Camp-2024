eeb44bdd6aa3cc87ab647a543b306a650d6f1e67
ThiaraAdrian <thiara.adrian@gmail.com>
2024-07-31 10:12:06 +0200

    verlinken imprint + privacy-policy

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
