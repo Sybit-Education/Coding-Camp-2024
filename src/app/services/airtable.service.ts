3380781ec8b64383a574daf4b114555863e07be7
Alexander, Kugler <alexander.kugler@sybit.de>
2024-07-31 09:11:07 +0200

    fix: format service

--- src/app/services/airtable.service.ts
+++ src/app/services/airtable.service.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {Injectable} from '@angular/core';
-import {forkJoin, from, map, Observable, Subject, switchMap, of} from "rxjs";
+import {forkJoin, from, map, Observable, of, Subject, switchMap} from "rxjs";
 import {Activity} from "../types/activity.interface";
 import {TypesInterface} from "../types/types.interface";
 import {environment} from "../../environments/environment";
@@ -8,7 +8,7 @@ import {HttpClient} from "@angular/common/http";
 
 
 @Injectable({
-	providedIn: "root",
+    providedIn: "root",
 })
 export class AirtableService {
 
@@ -18,47 +18,6 @@ export class AirtableService {
 
     constructor(private http: HttpClient) {
         this.fetchActivities()
-    }
-
-    private fetchActivities() {
-        forkJoin([
-            this.getTypeList(),
-            this.getMediaList()
-        ]).pipe(
-            switchMap(([types, medias]) => {
-                return from(this.http.get<Activity[]>(environment.apiUrl + '/activities')).pipe(
-                    map((records: any) => {
-                        return records.map((record: any) => {
-                            return {
-                                name: record.fields['name'] as string,
-                                description: record.fields['description'] as string,
-                                type: types.find((type: any) => type.id === (record.fields?.['type']?.[0])) as TypesInterface,
-                                street: record.fields['street'] as string,
-                                number: record.fields['number'] as string,
-                                zip: record.fields['zip'] as string,
-                                city: record.fields['city'] as string,
-                                latitude: record.fields['latitude'] as number,
-                                longitude: record.fields['longitude'] as number,
-                                website: record.fields['website'] as string,
-                                osm_id: record.fields['osm_id'] as string,
-                                media: medias.find((media: any) => media.id === (record.fields?.['media']?.[0])) as any,
-                                age_restriction: record.fields['age_restriction'] as number,
-                                barrier_free: record.fields['barrier_free'] as boolean,
-                            } as Activity
-                        }) as Activity[]
-                    })
-                )
-            })
-        ).subscribe({
-            next: (response) => {
-                response.forEach(activity => this.activities.push(activity))
-                this.subjects.forEach(subject => subject.next(response))
-                this.loaded = true;
-            },
-            error: (error) => {
-                console.error(error)
-            }
-        })
     }
 
     getActivityList(): Observable<Activity[]> {
@@ -77,33 +36,62 @@ export class AirtableService {
         }))
     }
 
-	getTypeList(): Observable<any> {
-		return from(this.http.get<Activity[]>(environment.apiUrl + "/types")).pipe(
-			map((records) => {
-				return records.map((record: any) => {
-					return {
-						id: record["id"] as string,
-						name: record.fields["name"] as string,
-						color: record.fields["color"] as string,
-					};
-				});
-			}),
-		);
-	}
+    getTypeList(): Observable<any> {
+        return from(this.http.get<Activity[]>(environment.apiUrl + "/types")).pipe(map((records) => {
+            return records.map((record: any) => {
+                return {
+                    id: record["id"] as string,
+                    name: record.fields["name"] as string,
+                    color: record.fields["color"] as string,
+                };
+            });
+        }),);
+    }
 
-	getMediaList(): Observable<any> {
-		return from(this.http.get<Activity[]>(environment.apiUrl + "/media")).pipe(
-			map((records) => {
-				return records.map((record: any) => {
-					return {
-						id: record["id"] as string,
-						activity: record.fields["activity"] as string,
-						file: record.fields["file"] as any,
-						license_text: record.fields["license_text"] as string,
-						source: record.fields["source"] as string,
-					};
-				});
-			}),
-		);
-	}
+    getMediaList(): Observable<any> {
+        return from(this.http.get<Activity[]>(environment.apiUrl + "/media")).pipe(map((records) => {
+            return records.map((record: any) => {
+                return {
+                    id: record["id"] as string,
+                    activity: record.fields["activity"] as string,
+                    file: record.fields["file"] as any,
+                    license_text: record.fields["license_text"] as string,
+                    source: record.fields["source"] as string,
+                };
+            });
+        }),);
+    }
+
+    private fetchActivities() {
+        forkJoin([this.getTypeList(), this.getMediaList()]).pipe(switchMap(([types, medias]) => {
+            return from(this.http.get<Activity[]>(environment.apiUrl + '/activities')).pipe(map((records: any) => {
+                return records.map((record: any) => {
+                    return {
+                        name: record.fields['name'] as string,
+                        description: record.fields['description'] as string,
+                        type: types.find((type: any) => type.id === (record.fields?.['type']?.[0])) as TypesInterface,
+                        street: record.fields['street'] as string,
+                        number: record.fields['number'] as string,
+                        zip: record.fields['zip'] as string,
+                        city: record.fields['city'] as string,
+                        latitude: record.fields['latitude'] as number,
+                        longitude: record.fields['longitude'] as number,
+                        website: record.fields['website'] as string,
+                        osm_id: record.fields['osm_id'] as string,
+                        media: medias.find((media: any) => media.id === (record.fields?.['media']?.[0])) as any,
+                        age_restriction: record.fields['age_restriction'] as number,
+                        barrier_free: record.fields['barrier_free'] as boolean,
+                    } as Activity
+                }) as Activity[]
+            }))
+        })).subscribe({
+            next: (response) => {
+                response.forEach(activity => this.activities.push(activity))
+                this.subjects.forEach(subject => subject.next(response))
+                this.loaded = true;
+            }, error: (error) => {
+                console.error(error)
+            }
+        })
+    }
 }
