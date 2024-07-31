1d34ce210882db9b2aaa925b69d8756c491a16f6
Alexander, Kugler <alexander.kugler@sybit.de>
2024-07-31 15:00:49 +0200

    fix: path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,23 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from "./features/home/home.component";
 import {DetailComponent} from "./components/detail/detail.component";
 import {AboutUsComponent} from "./features/about-us/about-us.component";
-
 import {ImprintComponent} from './features/imprint/imprint.component';
 import {PrivacyPolicyComponent} from './features/privacy-policy/privacy-policy.component';
-
+import {FavoritesComponent} from "./features/favorites/favorites.component";
+import {MapComponent} from "./features/map/map.component";
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'map',
+        component: MapComponent
+    },
+    {
+        path: 'favorites',
+        component: FavoritesComponent
     },
     {
         path: 'activity-details/:osm_id',
@@ -21,13 +29,11 @@ export const routes: Routes = [
         component: AboutUsComponent
     },
     {
-
         path: "imprint",
         component: ImprintComponent
     },
     {
         path: 'privacy-policy',
         component: PrivacyPolicyComponent
-
     }
 ];
