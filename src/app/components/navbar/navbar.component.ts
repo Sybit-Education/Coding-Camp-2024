91d8cf4884f70a14135df14210799e0ca4759c39
RafaeloxMC <53903394+RafaeloxMC@users.noreply.github.com>
2024-08-02 11:35:09 +0200

    Update navbar.component.ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,6 +4,8 @@ import { GetLuckyComponent } from "../get-lucky/get-lucky.component";
 import { Router, RouterLink, RouterModule } from "@angular/router";
 import { NgIf, NgOptimizedImage } from "@angular/common";
 import { FilterComponent } from "../filter/filter.component";
+import { BehaviorSubject } from 'rxjs';
+import { TypesInterface } from '../../types/types.interface';
 
 @Component({
     selector: 'app-navbar',
@@ -25,6 +27,7 @@ export class NavbarComponent {
     isRootUrl = false;
     isMapUrl = false;
     isHamburgerMenuActive = false;
+    filterSubject = new BehaviorSubject<TypesInterface[]>([])
 
     constructor(private router: Router) {
         this.router.events.subscribe(() => {
@@ -48,5 +51,9 @@ export class NavbarComponent {
 
     showHamburgerMenu() {
         this.isHamburgerMenuActive = !this.isHamburgerMenuActive
+    }
+
+    applyActiveFilters(filters: TypesInterface[]) {
+        this.filterSubject.next(filters);
     }
 }
