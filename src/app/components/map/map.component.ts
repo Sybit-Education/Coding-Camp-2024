8c9150a72364b3064acaf3ac5c1e65fd95a39d7c
RafaeloxMC <53903394+RafaeloxMC@users.noreply.github.com>
2024-08-02 09:47:18 +0200

    Update map.component.ts

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Map, MapBrowserEvent, View} from 'ol';
+import {Map, View} from 'ol';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import {OSM, Vector as VectorSource} from 'ol/source';
 import Feature from 'ol/Feature';
